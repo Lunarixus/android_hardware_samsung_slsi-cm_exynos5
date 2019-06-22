a5489228585f87763b317ea15308f3196db72582
Lunarixus <Nathanb644@hotmail.co.uk>
2019-06-22 21:08:55 +0000

    Revert "exynos5: Use generated kernel headers"
    This reverts commit e67edf2c0b9a8087091734c1e29b4c39fe26a9a1.

--- libhwcService/Android.mk
+++ libhwcService/Android.mk
@@ -28,6 +28,7 @@ LOCAL_CFLAGS += -DLOG_TAG=\"HWCService\"
 LOCAL_CFLAGS += -DHWC_SERVICES
 
 LOCAL_C_INCLUDES := \
+	$(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include \
 	$(LOCAL_PATH)/../include \
 	$(TOP)/hardware/samsung_slsi-cm/exynos/include \
 	$(TOP)/hardware/samsung_slsi-cm/exynos/libhwcutils \
@@ -37,7 +38,8 @@ LOCAL_C_INCLUDES := \
 	$(TOP)/hardware/samsung_slsi-cm/$(TARGET_SOC)/libhwcmodule \
 	$(TOP)/system/core/libsync/include
 
-LOCAL_HEADER_LIBRARIES := generated_kernel_headers
+LOCAL_ADDITIONAL_DEPENDENCIES := \
+	INSTALLED_KERNEL_HEADERS
 
 ifeq ($(BOARD_HDMI_INCAPABLE), true)
 LOCAL_C_INCLUDES += $(TOP)/hardware/samsung_slsi-cm/exynos/libhdmi_dummy
